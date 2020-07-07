1e9cbc429c910d5955d5188ca9a9805be9475002
Christina Wang <cywang117@outlook.com>
2020-07-07 09:54:47 +0900

    Complete backend routes & db methods with tests

--- db/knex.js
+++ db/knex.js
@@ -1,3 +1,7 @@
 const environment = process.env.NODE_ENV || 'development';
 const config = require('../knexfile')[environment];
+// https://github.com/brianc/node-pg-types/blob/master/lib/textParsers.js#L128
+const types = require('pg').types;
+const moment = require('moment');
+types.setTypeParser(1082, str => moment.utc(str).format());
 module.exports = require('knex')(config);
